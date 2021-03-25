64bb2fc5184802c82bd2e41e3607a31e73fd528c
manishkkk18 <67425704+manishkkk18@users.noreply.github.com>
2021-03-25 16:32:05 +0530

    Add files via upload

---
+++ bkynktilt.ino
@@ -0,0 +1,106 @@
+
+#include<Wire.h>
+#include <SPI.h>
+#define BLYNK_PRINT Serial
+#include <Blynk.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+ 
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+#define OLED_RESET    -1  // Reset pin # (or -1 if sharing reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+ 
+char auth[] = "WIybExOwyAgmW1s79gXaJnl9nvDODg5F";       // You should get Auth Token in the Blynk App.
+char ssid[] = "freedom 252";                       // Your WiFi credentials.
+char pass[] = "Mnk@1997";
+ 
+const int MPU_addr=0x68;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+ 
+int minVal=265;
+int maxVal=402;
+ 
+double x;
+double y;
+double z;
+ 
+void setup(){
+Wire.begin();
+Wire.beginTransmission(MPU_addr);
+Wire.write(0x6B);
+Wire.write(0);
+Wire.endTransmission(true);
+Serial.begin(9600);
+Blynk.begin(auth, ssid, pass);
+ 
+if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+{ 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+}
+ display.display();
+ delay(2);
+ display.clearDisplay();
+ 
+ display.clearDisplay();
+ display.setTextColor(WHITE);
+ display.setTextSize(2);
+ display.setCursor(0,5);
+ display.print("MPU Angles");
+ display.display();
+ delay(3000);
+ 
+}
+void loop(){
+Blynk.run();
+Wire.beginTransmission(MPU_addr);
+Wire.write(0x3B);
+Wire.endTransmission(false);
+Wire.requestFrom(MPU_addr,14,true);
+AcX=Wire.read()<<8|Wire.read();
+AcY=Wire.read()<<8|Wire.read();
+AcZ=Wire.read()<<8|Wire.read();
+int xAng = map(AcX,minVal,maxVal,-90,90);
+int yAng = map(AcY,minVal,maxVal,-90,90);
+int zAng = map(AcZ,minVal,maxVal,-90,90);
+ 
+x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+ 
+Serial.print("AngleX= ");
+Serial.println(x);
+ 
+Serial.print("AngleY= ");
+Serial.println(y);
+ 
+Serial.print("AngleZ= ");
+Serial.println(z);
+Serial.println("-----------------------------------------");
+ 
+display.clearDisplay();
+display.setTextSize(2);
+display.setCursor(0,0);
+display.print("X: ");
+display.println(x);
+ 
+display.setTextSize(2);
+display.setCursor(0,20);
+display.print("Y: ");
+display.println(y);
+ 
+display.setTextSize(2);
+display.setCursor(0,40);
+display.print("Z: ");
+display.println(z);
+display.display();
+ 
+Blynk.virtualWrite(V2, x);
+Blynk.virtualWrite(V3, y);
+Blynk.virtualWrite(V4, z);
+ 
+delay(500);
+}
